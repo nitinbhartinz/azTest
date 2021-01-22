b80388d23a7d4ba1b3462762ab50a47f9153080f
nitinbhartinz <37426387+nitinbhartinz@users.noreply.github.com>
2021-01-22 13:57:43 +1300

    Add files via upload

---
+++ tf/variables.tf
@@ -0,0 +1,8 @@
+variable "resource-group-name" {
+    default = "iac-terraform-test"
+    description = "Just an IAC test"
+}
+variable "location" {
+    default = "australiaeast"
+    description = " Deafult location for resources"
+}
